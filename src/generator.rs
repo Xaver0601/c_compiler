31594cfe3a4d8a501170fa04b21353c30a2c5d02
Xaver Riedel <xaverriedel@gmail.com>
2026-03-21 22:34:26 +0100

    separate generator

---
+++ src/generator.rs
@@ -0,0 +1,68 @@
+use crate::ast;
+
+pub struct Generator {
+  pub ast: ast::Program,
+}
+
+impl Generator {
+  pub fn generate_program(&self) -> String {
+    let mut assembly = String::new();
+    for fun in &self.ast.child_functions {
+      assembly += &format!(".globl {name}\n{name}:\n", name = fun.name);
+      assembly += &format!("{}", Self::generate_statement(&fun));
+    }
+    assembly += "ret\n.section .note.GNU-stack,\"\",@progbits\n";
+    assembly
+  }
+
+  fn generate_statement(fun: &ast::Function) -> String {
+    let mut stmt = String::new();
+    for stm in &fun.child_statements {
+      match stm {
+        ast::Statement::Return(x) => {
+          stmt += &Self::generate_expression(x);
+          // stmt += "movl %eax, %eax\n";
+        }
+        ast::Statement::Expression(x) => {
+          stmt += &Self::generate_expression(x);
+        }
+      }
+    }
+    stmt
+  }
+
+  fn generate_expression(expr: &ast::Expr) -> String {
+    match expr {
+      ast::Expr::LiteralInt(val) => format!("movl ${}, %eax\n", val),
+      ast::Expr::UnOp(op, operand) => {
+        let mut asm = String::new();
+        asm += &Self::generate_expression(operand);
+        match op {
+          ast::UnaryOp::Negate => {
+            asm += "negl %eax\n";
+          }
+          ast::UnaryOp::BitwiseNot => {
+            asm += "notl %eax\n";
+          }
+          ast::UnaryOp::LogicalNot => {
+            asm += "cmpl $0, %eax\n"; // if (%eax - 0) == 0 this sets the ZF flag to 1
+            asm += "movl $0, %eax\n"; // zero out %eax
+            asm += "sete %al\n"; // sete sets value to 1 if ZF flag is 1; can only modify 1 byte, so we use %al (lower byte of %eax)
+          }
+        }
+        asm
+      }
+      ast::Expr::BinOp(op, operand1, operand2) => {
+        let mut asm = String::new();
+        asm += &Self::generate_expression(operand1);
+        match op {
+          ast::BinaryOp::Add => {}
+          ast::BinaryOp::Subtract => {}
+          ast::BinaryOp::Multiply => {}
+          ast::BinaryOp::Divide => {}
+        }
+        asm
+      }
+    }
+  }
+}
